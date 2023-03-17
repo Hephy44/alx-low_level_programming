6c7f4b71b8c911a86c767bdc3ed1a28124e1c498
Hephy44 <hephyma@gmail.com>
2023-03-17 03:19:04 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - main fuction
+ *Return: returns to 0
+ */
+ 
+int main(void)
+{	
+	printf("size of char: %lu byte(s)", sizeof(char)\n);
+	printf("size of int: %lu byte(s)", sizeof(int)\n);
+	printf("size of long int: %lu byte(s)", sizeof(long int)\n);
+	printf("size of long long int: %lu byte(s)", sizeof(long long int)\n);
+	printf("size of float: %lu byte(s)", sizeof(float)\n);
+	return (0);
+}
